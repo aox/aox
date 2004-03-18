6f8ad2a8f730325c36bf28c43a7254a3074de0f9
Abhijit Menon-Sen <ams@wiw.org>
2004-03-18 08:59:40 +0000

    1. NOOP acquires some database-querying code for testing.    (We'll need some of it later anyway.)
    p4raw-id: //oryx/src/main@452

--- imapd/handlers/noop.cpp
+++ imapd/handlers/noop.cpp
@@ -1,12 +1,23 @@
+/*! \class Noop noop.h
+    \brief NOOP (RFC 3501, §6.1.2)
+
+    One might surmise that this function is a true noop, but it's not.
+    The side effects need to be handled somehow.
+*/
+
 #include "noop.h"
 
+// XXX: We should only need to include one header (database) here.
+#include "cache.h"
+#include "database.h"
 
-/*! One might surmise that this function is a true noop, but it's
-    not. The side effects need to be handled somehow.
-*/
+#include "string.h"
 
 void Noop::execute()
 {
-    // executing a noop is very simple.
+    // This is just for testing. AMS 20040318
+    Query *q = new Query( "" );
+    Database::query( q );
+
     setState( Finished );
 }
