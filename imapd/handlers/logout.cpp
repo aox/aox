e511b75ef3068a0d84203151a4de785253361e1e
Abhijit Menon-Sen <ams@wiw.org>
2004-07-05 15:09:21 +0000

    Urk. Merge some fiddling back in.
    p4raw-id: //oryx/src/main@1223

--- imapd/handlers/logout.cpp
+++ imapd/handlers/logout.cpp
@@ -7,12 +7,11 @@
     Terminates an IMAP session (RFC 3501, §6.1.3)
 */
 
-
-/*! Carries out the actual logout. */
+/*! \reimp */
 
 void Logout::execute()
 {
     respond( "BYE" );
     imap()->setState( IMAP::Logout );
-    setState( Finished );
+    finish();
 }
