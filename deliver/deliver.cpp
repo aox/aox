46844e430af92556954dd5c4bc492d83d201a73c
Abhijit Menon-Sen <ams@wiw.org>
2004-07-15 08:00:16 +0000

    An LMTP-based deliver. (Untested)
    p4raw-id: //oryx/src/main@1327

--- deliver/deliver.cpp
+++ deliver/deliver.cpp
@@ -1,88 +1,40 @@
-#include "log.h"
-#include "string.h"
-#include "address.h"
-#include "parser.h"
-#include "buffer.h"
-#include "message.h"
-#include "configuration.h"
-#include "date.h"
-#include "header.h"
-#include "injector.h"
-#include "mailbox.h"
 #include "arena.h"
 #include "scope.h"
 #include "test.h"
-#include "log.h"
+#include "configuration.h"
 #include "logclient.h"
-#include "occlient.h"
-#include "database.h"
+#include "log.h"
 #include "file.h"
+#include "header.h"
+#include "message.h"
+#include "smtpclient.h"
 #include "loop.h"
 
-#include <time.h> // XXX this needs to go. fix setTimeout().
 #include <stdlib.h>
 
 
-class DeliveryDbClient: public EventHandler
-{
-public:
-    DeliveryDbClient();
-    void execute();
+/*! \nodoc */
 
-    Injector * injector;
-};
-
-
-DeliveryDbClient::DeliveryDbClient()
-    : EventHandler(), injector( 0 )
-{
-}
-
-
-void DeliveryDbClient::execute()
-{
-    if ( injector && injector->done() )
-        Loop::shutdown();
-}
-
-
-int main( int argc, char ** argv )
+int main( int argc, char *argv[] )
 {
     Arena firstArena;
     Scope global( &firstArena );
 
-    Test::runTests();
+    Configuration::makeGlobal( ".deliverrc" );
 
     Log l( Log::Immediate );
     global.setLog( &l );
     LogClient::setup();
 
-    OCClient::setup();
-    Database::setup();
-    Mailbox::setup();
+    File input( "/proc/self/fd/0", File::Read );
+    String contents = input.contents();
+    Message * m = new Message( contents, true );
 
-    log( "delivery agent started" );
-    log( Test::report() );
-
-    Configuration::global()->report();
-    l.commit();
-
-    if ( Log::disastersYet() )
-        exit( 1 );
-
-    File input( "/proc/fd/self/stdin", File::Read ); // ### evil hack
-    String contents = input.contents(); // ### use Buffer instead
-    Message * m = new Message( contents, true ); // <- be strict? really?
-    Mailbox * inbox = Mailbox::find( argv[1] );
-    if ( !inbox ) {
+    if ( !m || !m->valid() )
         exit( -1 );
-    }
-    List<Mailbox> * inboxes = new List<Mailbox>;
-    inboxes->append( inbox );
-    
-    DeliveryDbClient * helper = new DeliveryDbClient;
-    Injector * i = new Injector( m, inboxes, helper );
-    helper->injector = i;
+
+    Address *a = m->header()->addresses( HeaderField::To )->first();
+    (void)new SmtpClient( m, a );
 
     Loop::start();
 }
