d3e1a75d7d3f2f52aae99bf66d4f44d236669c6a
Arnt Gulbrandsen <arnt@oryx.com>
2004-10-07 13:57:39 +0000

    the namespace handler emitted a syntactically wrong response.
    p4raw-id: //oryx/src/main@1784

--- imapd/handlers/namespace.cpp
+++ imapd/handlers/namespace.cpp
@@ -4,6 +4,11 @@
 
 /*! \class Namespace namespace.h
     Implements the NAMESPACE extension specified in RFC 2342.
+
+    Mailstore uses a single namespace, and this command informs the
+    client about how this space is set up. Notably,
+    /users/<name>/... is the same as ..., and other users are in
+    /users/.
 */
 
 
@@ -14,7 +19,7 @@ void Namespace::execute()
     String personal, other, shared;
 
     personal = "((\"\" \"/\") (\"/users/"+ imap()->login() +"/\" \"/\"))";
-    other    = "((\"/users/\"  \"/\"))";
+    other    = "((\"/users/\" \"/\"))";
     shared   = "((\"/\" \"/\"))";
 
     respond( "NAMESPACE " + personal + " " + other + " " + shared );
