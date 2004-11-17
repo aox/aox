f1266594fcef9515a815858d16a2ac0ce278fded
Arnt Gulbrandsen <arnt@oryx.com>
2004-11-17 17:01:23 +0000

    move user.cpp to server/user.cpp make documentation for user
    p4raw-id: //oryx/src/main@1966

---
+++ server/user.cpp
@@ -0,0 +1,423 @@
+#include "user.h"
+
+#include "address.h"
+#include "mailbox.h"
+#include "query.h"
+#include "configuration.h"
+#include "transaction.h"
+#include "addresscache.h"
+
+
+class UserData
+{
+public:
+    UserData(): id( 0 ), inbox( 0 ), address( 0 ),
+                q( 0 ), t( 0 ), user( 0 ),
+                mode( LoungingAround )
+        {}
+    String login;
+    String secret;
+    uint id;
+    Mailbox * inbox;
+    Address * address;
+    Query * q;
+    Transaction * t;
+    EventHandler * user;
+    String error;
+
+    enum Operation {
+        LoungingAround,
+        Creating,
+        Renaming,
+        Refreshing,
+        Removing
+    };
+    Operation mode;
+};
+
+
+/*! \class User user.h
+
+    The User class models a single Mailstore user, which may be able
+    to log in, own Mailbox objects, etc.
+*/
+
+
+/*! Constructs an empty User. The result does not map to anything in
+    the database.
+*/
+
+User::User()
+    : d( new UserData )
+{
+    // nothing
+}
+
+
+/*! Sets this User object to have login \a string. The database is not
+    updated - \a string is not used except to create Query objects
+    during e.g. refresh().
+*/
+
+void User::setLogin( const String & string )
+{
+    d->login = string;
+}
+
+
+/*! Returns the User's login string, which is an empty string
+    initially and is set up by refresh().
+*/
+
+String User::login() const
+{
+    return d->login;
+}
+
+
+/*! Sets this User to have \a secret as password. The database isn't
+    updated unless e.g. create() is called.
+*/
+
+void User::setSecret( const String & secret )
+{
+    d->secret = secret;
+}
+
+
+/*! Returns the User's secret (password), which is an empty string
+    until refresh() has fetched the database contents.
+*/
+
+String User::secret() const
+{
+    return d->secret;
+}
+
+
+/*! Returns a pointer to the user's inbox, or a null pointer if this
+    object doesn't know it or if the user has none.
+*/
+
+Mailbox * User::inbox() const
+{
+    return d->inbox;
+}
+
+
+/*! Sets this User object to have address \a a. The database is not
+    updated - \a a is not used except maybe to search in refresh().
+*/
+
+void User::setAddress( Address * a )
+{
+    d->address = a;
+}
+
+
+/*! Returns the address belonging to this User object, or a null
+    pointer if this User has no Address.
+*/
+
+Address * User::address()
+{
+    if ( !d->address ) {
+        String dom = Configuration::hostname();
+        uint i = dom.find( '.' );
+        if ( i > 0 )
+            dom = dom.mid( i+1 );
+        d->address = new Address( "", d->login, dom );
+    }
+    return d->address;
+}
+
+
+static PreparedStatement * psl;
+static PreparedStatement * psa;
+
+
+/*! Starts refreshing this object from the database, and remembers to
+    call \a user when the refresh is complete.
+*/
+
+void User::refresh( EventHandler * user )
+{
+    if ( d->q )
+        return;
+    d->user = user;
+    if ( !psl ) {
+        psl = new PreparedStatement( "select "
+                                     "users.id, users.address, users.inbox,"
+                                     "users.parentspace, "
+                                     "users.login, users.secret, "
+                                     "addresses.name, addresses.localpart, "
+                                     "addresses.domain "
+                                     "from users,addresses where "
+                                     "users.login=$1 and"
+                                     "users.id=addresses.id" );
+        psa = new PreparedStatement( "select "
+                                     "users.id, users.address, users.inbox,"
+                                     "users.parentspace, "
+                                     "users.login, users.secret, "
+                                     "addresses.name, addresses.localpart, "
+                                     "addresses.domain "
+                                     "from users,addresses where "
+                                     "users.address=addresses.id and "
+                                     "addresses.name=$1 and"
+                                     "addresses.localpart=$2 and"
+                                     "lower(addresses.domain)=$3" );
+    }
+    if ( !d->login.isEmpty() ) {
+        d->q = new Query( *psl, this );
+        d->q->bind( 1, d->login );
+    }
+    else if ( d->address ) {
+        d->q = new Query( *psa, this );
+        d->q->bind( 1, d->address->name() );
+        d->q->bind( 2, d->address->localpart() );
+        d->q->bind( 3, d->address->domain().lower() );
+    }
+    if ( d->q )
+        d->q->execute();
+    else
+        user->notify();
+}
+
+
+/*! Parses the query results for refresh(). */
+
+void User::refreshHelper()
+{
+    if ( !d->q || !d->q->done() )
+        return;
+    Row * r;
+    bool any = false;
+    while ( (r=d->q->nextRow()) !=0 ) {
+        any = true;
+        d->id = r->getInt( "users.id" );
+        d->login = r->getString( "users.login" );
+        d->secret = r->getString( "users.secret" );
+        d->inbox = Mailbox::find( r->getInt( "users.inbox" ) );
+        // ignore parentspace for now
+        d->address = new Address;
+    }
+    if ( d->user )
+        d->user->notify();
+}
+
+
+void User::execute()
+{
+    switch( d->mode ) {
+    case UserData::Creating:
+        createHelper();
+        break;
+    case UserData::Renaming:
+        renameHelper();
+        break;
+    case UserData::Refreshing:
+        refreshHelper();
+        break;
+    case UserData::Removing:
+        removeHelper();
+        break;
+    case UserData::LoungingAround:
+        break;
+    }
+}
+
+
+/*! Creates this user in the database notifies \a user afterwards. If
+    the user could not be created, error() returns a message about
+    what went wrong.
+*/
+
+void User::create( EventHandler * user )
+{
+    if ( !user )
+        return;
+
+    if ( exists() ) {
+        d->error = "User exists already";
+        user->notify();
+        return;
+    }
+
+    if ( !valid() ) {
+        user->notify();
+        return;
+    }
+
+    d->t = new Transaction( this );
+    d->mode = UserData::Creating;
+    createHelper();
+}
+
+
+/*! This private function carries out create() work on behalf of
+    execute().
+*/
+
+void User::createHelper()
+{
+    Address * a = address();
+    if ( !a->id() ) {
+        // note: this doesn't wrap the address insert in our transaction
+        List<Address> l;
+        l.append( a );
+        AddressCache::lookup( &l, this );
+        return;
+    }
+
+    if ( !d->q ) {
+        d->q = new Query( "insert into mailboxes (name) values "
+                          "( (select name from namespaces where id="
+                          "   (select max(id) from namespaces)) ||"
+                          "  '/' || $1 || '/inbox' )",
+                          this );
+        d->q->bind( 1, d->login );
+        d->t->enqueue( d->q );
+
+        Query * q2
+            = new Query( "insert into users "
+                         "(address,inbox,parentspace,login,secret)"
+                         "values ($1,"
+                         "(select id from mailboxes where name="
+                         " (select name from namespaces where id="
+                         "   (select max(id) from namespaces)) ||"
+                         "  '/' || $2 || '/inbox' ),"
+                         "(select name from namespaces where id="
+                         "   (select max(id) from namespaces)),"
+                         "$2,$3)",
+                         this );
+        q2->bind( 1, a->id() );
+        q2->bind( 2, d->login );
+        q2->bind( 4, d->secret );
+        d->t->enqueue( q2 );
+        d->t->execute();
+    }
+    if ( !d->t->done() )
+        return;
+    if ( d->t->failed() )
+        d->error = "SQL error: " + d->t->error();
+    d->user->notify();
+}
+
+
+/*! Returns true if this user is valid, that is, if it has the
+    information that must be present in order to write it to the
+    database and do not have defaults.
+
+    Sets error() if applicable.
+*/
+
+bool User::valid()
+{
+    if ( d->login.isEmpty() ) {
+        d->error = "Login name must be supplied";
+        return false;
+    }
+    if ( d->secret.isEmpty() ) {
+        d->error = "Login name <" + d->login + "> has no password";
+        return false;
+    }
+    if ( d->id )
+        return true;
+
+    return false;
+}
+
+
+/*! Returns a textual description of the last error seen, or a null
+    string if everything is in order. The string is set by valid() and
+    perhaps other functions.
+*/
+
+String User::error() const
+{
+    return d->error;
+}
+
+
+/*! Returns true if this user is known to exist in the database, and
+    false if it's unknown or doesn't exist.
+*/
+
+bool User::exists()
+{
+    return d->id > 0;
+}
+
+
+/*! Finishes the work of rename(). */
+
+void User::renameHelper()
+{
+    if ( !d->q->done() )
+        return;
+    if ( d->q->failed() ) {
+        d->error = "SQL error during user update: " + d->q->error();
+        refresh( d->user );
+    }
+    d->user->notify();
+}
+
+
+/*! Renames this User to \a newLogin and notifies \a user when the
+    operation is complete. exists() must be true to call this
+    function.
+*/
+
+void User::rename( const String & newLogin, EventHandler * user  )
+{
+    if ( !exists() ) {
+        d->error = "Cannot rename nonexistent user";
+        return;
+    }
+    d->q = new Query( "update users set login=$1 where id=$2", this );
+    d->q->bind( 1, newLogin );
+    d->q->bind( 2, d->id );
+    d->q->execute();
+    d->login = newLogin;
+}
+
+
+/*! Renames the password of this User to \a newSecret and notifies \a
+    user when the operation is complete. exists() must be true to call
+    this function.
+*/
+
+void User::changeSecret( const String & newSecret, EventHandler * user )
+{
+    if ( !exists() ) {
+        d->error = "Cannot set password for nonexistent user";
+        return;
+    }
+    d->q = new Query( "update users set secret=$1 where id=$2", this );
+    d->q->bind( 1, newSecret );
+    d->q->bind( 2, d->id );
+    d->q->execute();
+    d->secret = newSecret;
+}
+
+
+/*! Removes this user from the database and notifies \a user when the
+    operation is complete.
+*/
+
+void User::remove( EventHandler * user )
+{
+    if ( !exists() )
+        return;
+    d->q = new Query( "delete from users where id=$1", this );
+    d->q->bind( 1, d->id );
+}
+
+
+/*! Finishes the work of remove(). */
+
+void User::removeHelper()
+{
+    if ( d->q->done() )
+        d->id = 0;
+}
---
+++ server/user.h
@@ -0,0 +1,56 @@
+#ifndef USER_H
+#define USER_H
+
+#include "event.h"
+
+#include "string.h"
+#include "list.h"
+
+class Mailbox;
+class Address;
+class Query;
+
+
+class User: public EventHandler
+{
+public:
+    User();
+
+    void setLogin( const String & );
+    String login() const;
+
+    void setSecret( const String & );
+    String secret() const;
+
+    void setInbox( Mailbox * & );
+    Mailbox * inbox() const;
+
+    void setAddress( Address * );
+    Address * address();
+
+    void refresh( EventHandler * user );
+    void create( EventHandler * user );
+    void rename( const String & newLogin, EventHandler * user );
+    void changeSecret( const String & newSecret, EventHandler * user );
+    void remove( EventHandler * user );
+
+    bool valid();
+    bool exists();
+
+    String error() const;
+
+    static List<User> * users();
+
+    void execute();
+private:
+    void createHelper();
+    void renameHelper();
+    void refreshHelper();
+    void removeHelper();
+
+private:
+    class UserData * d;
+};
+
+
+#endif
