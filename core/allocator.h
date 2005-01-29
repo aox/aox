829b9910347b3d5cdd65bcb5131770d7d83627de
Arnt Gulbrandsen <arnt@oryx.com>
2005-01-29 21:48:40 +0000

    gulbrandsen gc, simplified until it's nearly there, and broken, too. specifically, the algorithm to go from arbitrary pointer to allocator can't really work. must think again.
    not added:
        a) addRoot(), to add a new root
        b) pointers-at-front setter (right now the entire object is
           treated as pointers)
        c) connection smarts, to track per-connection memory use
    
    this is called allocator because the name arena is in use and I
    didn't want to disturb the servers just yet. we can rename if that
    looks like a good idea.
    
    p4raw-id: //oryx/src/main@2526

---
+++ core/allocator.h
@@ -0,0 +1,47 @@
+// Copyright Oryx Mail Systems GmbH. All enquiries to info@oryx.com, please.
+
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+#include "global.h"
+
+
+class Allocator
+{
+public:
+    Allocator( uint );
+
+    void * allocate( uint size );
+
+    void * block( uint );
+
+    static uint rounded( uint size ) {
+        uint i = 0;
+        while ( 8U << i < size + 4 )
+            i++;
+        return (8U << i) - 4U;
+    }
+
+    static Allocator * allocator( uint size );
+
+    void *operator new( uint );
+    void operator delete( void * );
+
+    static void free();
+    static void addRoot( void ** );
+
+private:
+    uint step;
+    uint num;
+    uint capacity;
+    uint * bitmap;
+    void * buffer;
+    Allocator * next;
+
+private:
+    static void mark( void * );
+    void sweep();
+};
+
+
+#endif
