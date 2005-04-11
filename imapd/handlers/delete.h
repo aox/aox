b456d1e9a463a60b513a20b79d80f59206bb97a6
Arnt Gulbrandsen <arnt@oryx.com>
2005-04-11 18:08:00 +0000

    from old server
    p4raw-id: //oryx/src/main@50006

---
+++ imapd/handlers/delete.h
@@ -0,0 +1,24 @@
+// Copyright Oryx Mail Systems GmbH. All enquiries to info@oryx.com, please.
+
+#ifndef DELETE_H
+#define DELETE_H
+
+#include "command.h"
+
+
+class Delete
+    : public Command
+{
+public:
+    Delete();
+
+    void parse();
+    void execute();
+
+private:
+    String name;
+    class Query *q;
+};
+
+
+#endif
