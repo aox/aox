9fc129f9aeea5f7cd27769b76208c95d4992d957
Abhijit Menon-Sen <ams@wiw.org>
2005-04-27 09:58:15 +0000

    Split session away from imapsession, step #1.
    p4raw-id: //oryx/src/main@50191

---
+++ server/session.h
@@ -0,0 +1,72 @@
+// Copyright Oryx Mail Systems GmbH. All enquiries to info@oryx.com, please.
+
+#ifndef IMAPSESSION_H
+#define IMAPSESSION_H
+
+#include "global.h"
+#include "messageset.h"
+#include "permissions.h"
+#include "event.h"
+
+class Mailbox;
+class Message;
+class Select;
+class IMAP;
+
+
+class ImapSession {
+public:
+    ImapSession( Mailbox *, IMAP *, bool );
+    ~ImapSession();
+
+    IMAP * imap() const;
+    Mailbox * mailbox() const;
+    bool readOnly() const;
+
+    Permissions *permissions() const;
+    void setPermissions( Permissions * );
+    bool allows( Permissions::Right );
+
+    uint uidnext() const;
+    uint uidvalidity() const;
+
+    uint uid( uint ) const;
+    uint msn( uint ) const;
+    uint count() const;
+
+    uint firstUnseen() const;
+    void setFirstUnseen( uint );
+
+    void insert( uint );
+    void remove( uint );
+
+    MessageSet recent() const;
+    bool isRecent( uint ) const;
+    void addRecent( uint );
+
+    const MessageSet & expunged() const;
+    const MessageSet & messages() const;
+
+    bool responsesNeeded() const;
+    void emitResponses();
+    void updateUidnext();
+    void expunge( const MessageSet & );
+
+private:
+    class SessionData *d;
+};
+
+
+class ImapSessionInitializer: public EventHandler {
+public:
+    ImapSessionInitializer( ImapSession *, EventHandler * );
+
+    bool done() const;
+
+    void execute();
+private:
+    class ImapSessionInitializerData * d;
+};
+
+
+#endif
