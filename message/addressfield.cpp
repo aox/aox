8c62117d2658101ca3b1fe19e78868a8aac46e18
Arnt Gulbrandsen <arnt@oryx.com>
2005-06-18 13:01:15 +0000

    we're internet software. we use crlf.
    p4raw-id: //oryx/src/main@50467

--- message/addressfield.cpp
+++ message/addressfield.cpp
@@ -106,12 +106,12 @@ void AddressField::update()
 
         if ( t == HeaderField::References ) {
             wsep = " ";
-            lsep = "\n ";
+            lsep = "\r\n ";
             lpos = 1;
         }
         else {
             wsep = ", ";
-            lsep = ",\n    ";
+            lsep = ",\r\n    ";
             lpos = 4;
         }
 
@@ -122,7 +122,7 @@ void AddressField::update()
             if ( first ) {
                 first = false;
             }
-            else if ( c + wsep.length() + a.length() > 72 ) {
+            else if ( c + wsep.length() + a.length() > 71 ) {
                 s.append( lsep );
                 c = lpos;
             }
