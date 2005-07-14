b803f0a5c09fff77916f01770ee3e3712c276eb6
Abhijit Menon-Sen <ams@wiw.org>
2005-07-14 04:49:52 +0000

    Oops, I didn't mean that bit.
    p4raw-id: //oryx/src/main@50606

--- imapd/handlers/fetch.cpp
+++ imapd/handlers/fetch.cpp
@@ -777,11 +777,9 @@ String Fetch::bodyStructure( Multipart * m, bool extended )
         r.append( ")" );
     }
     else if ( ct && ct->type() == "message" && ct->subtype() == "rfc822" ) {
-        Bodypart *bp = (Bodypart *)m->parent();
-        if ( !bp )
+        Bodypart *bp = (Bodypart *)m;
+        if ( !bp->rfc822() && !m->parent() )
             bp = m->children()->first();
-        // Bodypart *bp = (Bodypart *)m;
-        // if ( !bp->rfc822() && !m->parent() )
         r = singlePartStructure( bp,  m->header(), extended );
     }
     else {
