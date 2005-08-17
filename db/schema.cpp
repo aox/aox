935e056d92a07402091271e2570f58b9a33cf621
Abhijit Menon-Sen <ams@wiw.org>
2005-08-17 12:32:17 +0000

    Nice new schema framework.
    p4raw-id: //oryx/src/main@50873

--- db/schema.cpp
+++ db/schema.cpp
@@ -16,16 +16,19 @@ class SchemaData
 public:
     SchemaData()
         : l( new Log( Log::Database ) ),
-          state( 0 ), lock( 0 ), t( 0 ),
-          result( 0 )
+          state( 0 ), substate( 0 ), revision( 0 ),
+          lock( 0 ), seq( 0 ), update( 0 ), q( 0 ), t( 0 ),
+          result( 0 ), upgrade( false )
     {}
 
     Log *l;
     int state;
-    Query *lock;
+    int substate;
+    int revision;
+    Query *lock, *seq, *update, *q;
     Transaction *t;
-
     Query *result;
+    bool upgrade;
 };
 
 
@@ -55,56 +58,138 @@ Schema::Schema( EventHandler * owner )
 void Schema::execute()
 {
     if ( d->state == 0 ) {
-        d->lock = new Query( "select revision from mailstore for update",
-                             this );
+        d->lock =
+            new Query( "select revision from mailstore for update", this );
         d->t->enqueue( d->lock );
-        d->t->commit();
+        d->t->execute();
         d->state = 1;
     }
 
     if ( d->state == 1 ) {
-        if ( !d->t->done() )
+        if ( !d->lock->done() )
             return;
 
         Row *r = d->lock->nextRow();
-        if ( d->lock->failed() || !r ) {
-            d->l->log( "Database inconsistent: "
-                       "Couldn't query the mailstore table.",
+        if ( r )
+            d->revision = r->getInt( "revision" );
+
+        if ( !r || d->lock->failed() ) {
+            d->l->log( "Bad database: Couldn't query the mailstore table.",
                        Log::Disaster );
-            d->state = 2;
             d->result->setState( Query::Failed );
-            d->result->notify();
-            return;
+            d->revision = currentRevision;
+            d->t->commit();
+            d->state = 7;
         }
-
-        int revision = r->getInt( "revision" );
-        if ( revision == currentRevision ) {
-            d->state = 2;
+        else if ( d->revision == currentRevision ) {
             d->result->setState( Query::Completed );
-            d->result->notify();
-            return;
+            d->t->commit();
+            d->state = 7;
+        }
+        else if ( d->upgrade && d->revision < currentRevision ) {
+            d->l->log( "Updating schema from revision " +
+                       fn( d->revision ) + " to revision " +
+                       fn( currentRevision ) );
+            d->state = 2;
+        }
+        else {
+            String s( "The existing schema (revision #" );
+            s.append( fn( d->revision ) );
+            s.append( ") is " );
+            if ( d->revision < currentRevision )
+                s.append( "older" );
+            else
+                s.append( "newer" );
+            s.append( " than this server (version " );
+            s.append( Configuration::compiledIn( Configuration::Version ) );
+            s.append( ") expected (revision #" );
+            s.append( fn( currentRevision ) );
+            s.append( "). Please " );
+            if ( d->revision < currentRevision )
+                s.append( "run 'ms migrate'" );
+            else
+                s.append( "upgrade" );
+            s.append( " or contact support." );
+            d->l->log( s, Log::Disaster );
+            d->result->setState( Query::Failed );
+            d->revision = currentRevision;
+            d->t->commit();
+            d->state = 7;
+        }
+    }
+
+    while ( d->revision < currentRevision ) {
+        if ( d->state == 2 ) {
+            d->seq =
+                new Query( "select nextval('revisions')::integer as seq",
+                           this );
+            d->t->enqueue( d->seq );
+            d->t->execute();
+            d->state = 3;
         }
 
-        String s( "The existing schema (revision #" );
-        s.append( fn( revision ) );
-        s.append( ") is " );
-        if ( revision < currentRevision )
-            s.append( "older" );
-        else
-            s.append( "newer" );
-        s.append( " than this server (version " );
-        s.append( Configuration::compiledIn( Configuration::Version ) );
-        s.append( ") expected (revision #" );
-        s.append( fn( currentRevision ) );
-        s.append( "). Please " );
-        if ( revision < currentRevision )
-            s.append( "run 'ms migrate'" );
-        else
-            s.append( "upgrade" );
-        s.append( " or contact support." );
-        d->l->log( s, Log::Disaster );
-        d->state = 2;
-        d->result->setState( Query::Failed );
+        if ( d->state == 3 ) {
+            if ( !d->seq->done() )
+                return;
+
+            int gap = d->seq->nextRow()->getInt( "seq" ) - d->revision;
+            if ( gap > 1 ) {
+                d->l->log( "Can't upgrade schema because an earlier "
+                           "attempt to do so failed.", Log::Disaster );
+                d->result->setState( Query::Failed );
+                d->revision = currentRevision;
+                d->t->commit();
+                d->state = 7;
+                break;
+            }
+            d->state = 4;
+        }
+
+        if ( d->state == 4 ) {
+            // ...
+        }
+
+        if ( d->state == 5 ) {
+            d->update =
+                new Query( "update mailstore set revision=revision+1",
+                           this );
+            d->t->enqueue( d->update );
+            d->t->execute();
+            d->state = 6;
+        }
+
+        if ( d->state == 6 ) {
+            if ( !d->update->done() )
+                return;
+
+            d->state = 2;
+            d->revision++;
+
+            if ( d->revision == currentRevision ) {
+                d->t->commit();
+                d->state = 8;
+                break;
+            }
+        }
+    }
+
+    if ( d->state == 7 || d->state == 8 ) {
+        if ( !d->t->done() )
+            return;
+
+        if ( d->t->failed() && !d->result->failed() ) {
+            d->result->setState( Query::Failed );
+            d->l->log( "The schema transaction failed.", Log::Disaster );
+        }
+        else if ( d->state == 8 ) {
+            d->result->setState( Query::Completed );
+            d->l->log( "Schema updated to revision " +fn( currentRevision ) );
+        }
+        d->state = 9;
+    }
+
+    if ( d->state == 9 ) {
+        d->state = 42;
         d->result->notify();
     }
 }
@@ -124,7 +209,6 @@ void Schema::execute()
 Query * Schema::check( EventHandler * owner )
 {
     Schema * s = new Schema( owner );
-
     s->execute();
 
     return s->d->result;
@@ -141,6 +225,9 @@ Query * Schema::check( EventHandler * owner )
 
 Query * Schema::upgrade( EventHandler * owner )
 {
-    // Not just yet.
-    return 0;
+    Schema * s = new Schema( owner );
+    s->d->upgrade = true;
+    s->execute();
+
+    return s->d->result;
 }
