0c76f0b4c3219b7f2d060dd03e8b5e5f8ebf6fa5
Abhijit Menon-Sen <ams@wiw.org>
2005-09-04 09:25:41 +0000

    An installer that can create DBUSER/DBNAME!
    p4raw-id: //oryx/src/main@50982

--- installer/installer.cpp
+++ installer/installer.cpp
@@ -2,21 +2,28 @@
 
 #include "scope.h"
 #include "string.h"
+#include "allocator.h"
 #include "stringlist.h"
+#include "configuration.h"
 #include "eventloop.h"
+#include "database.h"
+#include "entropy.h"
+#include "query.h"
+#include "event.h"
 #include "file.h"
+#include "md5.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
 
 
-StringList * args;
+class Dispatcher * d;
 bool report = false;
 
 
@@ -24,7 +31,6 @@ void error( String );
 bool exists( String );
 void oryxGroup();
 void oryxUser();
-void dbUser();
 void database();
 void configFile();
 
@@ -36,13 +42,12 @@ int main( int ac, char *av[] )
 {
     Scope global;
 
-    args = new StringList;
+    StringList args;
     while ( ac-- > 0 )
-        args->append( new String( *av++ ) );
-    args->shift();
-
-    while ( !args->isEmpty() ) {
-        String s( *args->shift() );
+        args.append( new String( *av++ ) );
+    args.shift();
+    while ( !args.isEmpty() ) {
+        String s( *args.shift() );
         if ( s == "-n" )
             report = true;
         else
@@ -51,6 +56,11 @@ int main( int ac, char *av[] )
 
     if ( getuid() != 0 )
         error( "Please run the installer as root." );
+
+    struct passwd * p = getpwnam( PGUSER );
+    if ( !p )
+        error( "PostgreSQL superuser '" PGUSER "' does not exist." );
+    seteuid( p->pw_uid );
 
     if ( report )
         printf( "Reporting what the installer needs to do.\n" );
@@ -65,13 +75,14 @@ int main( int ac, char *av[] )
                " - Not creating the Oryx schema.\n"
                " - Not creating stub configuration file." );
 
+    EventLoop::setup();
     oryxGroup();
     oryxUser();
-    dbUser();
     database();
-    configFile();
 
-    EventLoop::setup();
+    if ( d )
+        Allocator::addEternal( d, "dispatcher" );
+    EventLoop::global()->start();
 }
 
 
@@ -146,23 +157,126 @@ void oryxUser()
 }
 
 
-void dbUser()
+class Dispatcher
+    : public EventHandler
 {
-    if ( report ) {
-        printf( " - Create a PostgreSQL user named '" DBUSER "' "
-                "(e.g. \"create user " DBUSER "\" in psql).\n" );
-        return;
+public:
+    Query * q;
+    int state;
+
+    Dispatcher() : state( 0 ) {}
+    void execute()
+    {
+        database();
     }
-}
+};
 
 
 void database()
 {
-    if ( report ) {
-        printf( " - Create a database named '" DBNAME "' owned by '" DBUSER
-                "'\n   (e.g. \"create database " DBNAME " with owner " DBUSER
-                "\" in psql).\n" );
+    if ( !d ) {
+        Configuration::setup( "" );
+        Configuration::add( "db-user = '" PGUSER "'" );
+        Configuration::add( "db-name = 'template1'" );
+        Database::setup();
+        d = new Dispatcher;
+        d->q = new Query( "select usename from pg_catalog.pg_user where "
+                          "usename=$1", d );
+        d->q->bind( 1, DBUSER );
+        d->q->execute();
+    }
+
+    if ( !d->q->done() )
         return;
+
+    if ( d->state == 0 ) {
+        Row * r = d->q->nextRow();
+        if ( !r ) {
+            Entropy::setup();
+            String create( "create user " DBUSER " with encrypted password '" );
+            String passwd( DBPASS );
+            if ( passwd.isEmpty() ) {
+                if ( report )
+                    passwd = "(password here)";
+                else
+                    passwd = MD5::hash( Entropy::asString( 16 ) ).hex();
+            }
+            create.append( passwd );
+            create.append( "'" );
+
+            if ( report ) {
+                printf( " - Create a PostgreSQL user named '" DBUSER "'.\n"
+                        "   As user " PGUSER ", run:\n"
+                        "     psql -d template1 -qc \"%s\"\n", create.cstr() );
+                d->state = 2;
+            }
+            else {
+                d->state = 1;
+                d->q = new Query( create, d );
+                d->q->execute();
+            }
+        }
+        else {
+            d->state = 2;
+        }
+    }
+
+    if ( d->state == 1 ) {
+        if ( !d->q->done() )
+            return;
+        if ( d->q->failed() ) {
+            fprintf( stderr, "Couldn't create PostgreSQL user '" DBUSER "'. "
+                     "Please create it by hand and re-run the installer.\n" );
+            EventLoop::global()->shutdown();
+        }
+        d->state = 2;
+    }
+
+    if ( d->state == 2 ) {
+        d->state = 3;
+        d->q = new Query( "select datname from pg_catalog.pg_database where "
+                          "datname=$1", d );
+        d->q->bind( 1, DBNAME );
+        d->q->execute();
+    }
+
+    if ( d->state == 3 ) {
+        if ( !d->q->done() )
+            return;
+        Row * r = d->q->nextRow();
+        if ( !r ) {
+            String create( "create database " DBNAME " with owner " DBUSER " "
+                           "encoding 'UNICODE'" );
+            if ( report ) {
+                printf( " - Create a database named '" DBNAME "'.\n"
+                        "   As user " PGUSER ", run:\n"
+                        "     psql -d template1 -qc \"%s\"\n", create.cstr() );
+                d->state = 5;
+            }
+            else {
+                d->state = 4;
+                d->q = new Query( create, d );
+                d->q->execute();
+            }
+        }
+        else {
+            d->state = 5;
+        }
+    }
+
+    if ( d->state == 4 ) {
+        if ( !d->q->done() )
+            return;
+        if ( d->q->failed() ) {
+            fprintf( stderr, "Couldn't create database '" DBUSER "'. "
+                     "Please create it by hand and re-run the installer.\n" );
+            EventLoop::global()->shutdown();
+        }
+        d->state = 5;
+    }
+
+    if ( d->state == 5 ) {
+        configFile();
     }
 }
 
@@ -171,6 +285,7 @@ void configFile()
 {
     if ( report ) {
         printf( " - Generate a default configuration file.\n" );
-        return;
     }
+
+    EventLoop::global()->shutdown();
 }
