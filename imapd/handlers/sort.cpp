6c0d6017db144a32f9fdd97f9e3371e81667446f
Abhijit Menon-Sen <ams@wiw.org>
2005-09-29 17:49:03 +0000

    A parser for the non-search parts of SORT.
    p4raw-id: //oryx/src/main@51235

--- imapd/handlers/sort.cpp
+++ imapd/handlers/sort.cpp
@@ -12,6 +12,8 @@ public:
     {}
 
     bool uid;
+
+    String charset;
 };
 
 
@@ -31,6 +33,46 @@ Sort::Sort( bool u )
 
 void Sort::parse()
 {
+    space();
+    require( "(" );
+
+    bool atEnd = false;
+    while ( !atEnd ) {
+        bool reverse = false;
+        String item = letters( 1, 7 ).lower();
+
+        if ( item == "reverse" ) {
+            space();
+            item = letters( 1, 7 ).lower();
+            reverse = true;
+        }
+
+        if ( item == "arrival" )
+            ;
+        else if ( item == "cc" )
+            ;
+        else if ( item == "date" )
+            ;
+        else if ( item == "from" )
+            ;
+        else if ( item == "size" )
+            ;
+        else if ( item == "subject" )
+            ;
+        else if ( item == "to" )
+            ;
+        else
+            error( Bad, "Unknown SORT key: " + item );
+
+        if ( nextChar() == ' ' )
+            space();
+        else
+            atEnd = true;
+    }
+
+    require( ")" );
+    space();
+    d->charset = astring();
     end();
 }
 
