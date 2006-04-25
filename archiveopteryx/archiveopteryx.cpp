fbfd72b62cddc4b42973ea85c03be363d93d116f
Arnt Gulbrandsen <arnt@oryx.com>
2006-04-25 13:33:06 +0000

    ... and now with the required editing so that it answers all protocols
    p4raw-id: //oryx/src/main@51930

--- archiveopteryx/archiveopteryx.cpp
+++ archiveopteryx/archiveopteryx.cpp
@@ -1,20 +1,23 @@
 // Copyright Oryx Mail Systems GmbH. All enquiries to info@oryx.com, please.
 
 #include "scope.h"
-#include "logclient.h"
-#include "occlient.h"
-#include "database.h"
+#include "server.h"
+
+#include "pop.h"
+#include "imap.h"
+#include "http.h"
+#include "smtp.h"
+#include "sieve.h"
+
+#include "tls.h"
+#include "flag.h"
+#include "schema.h"
 #include "mailbox.h"
 #include "listener.h"
-#include "imap.h"
-#include "handlers/capability.h"
+#include "database.h"
+#include "occlient.h"
 #include "fieldcache.h"
 #include "addresscache.h"
-#include "server.h"
-#include "flag.h"
-#include "tls.h"
-#include "injector.h"
-#include "schema.h"
 
 
 /*! \nodoc */
@@ -23,7 +26,7 @@ int main( int argc, char *argv[] )
 {
     Scope global;
 
-    Server s( "imapd", argc, argv );
+    Server s( "archiveopteryx", argc, argv );
     s.setup( Server::Report );
 
     Listener< IMAP >::create(
@@ -34,6 +37,31 @@ int main( int argc, char *argv[] )
     Listener< IMAPS >::create(
         "IMAPS", Configuration::toggle( Configuration::UseImaps ),
         Configuration::ImapsAddress, Configuration::ImapsPort,
+        false
+    );
+    Listener< POP >::create(
+        "POP3", Configuration::toggle( Configuration::UsePop ),
+        Configuration::PopAddress, Configuration::PopPort,
+        false
+    );
+    Listener< HTTP >::create(
+        "HTTP", Configuration::toggle( Configuration::UseHttp ),
+        Configuration::HttpAddress, Configuration::HttpPort,
+        false
+    );
+    Listener< Sieve >::create(
+        "Sieve", Configuration::toggle( Configuration::UseSieve ),
+        Configuration::SieveAddress, Configuration::SievePort,
+        false
+    );
+    Listener< SMTP >::create(
+        "SMTP", Configuration::toggle( Configuration::UseSmtp ),
+        Configuration::SmtpAddress, Configuration::SmtpPort,
+        false
+    );
+    Listener< LMTP >::create(
+        "LMTP", Configuration::toggle( Configuration::UseLmtp ),
+        Configuration::LmtpAddress, Configuration::LmtpPort,
         false
     );
 
@@ -50,6 +78,8 @@ int main( int argc, char *argv[] )
     FieldNameCache::setup();
     Flag::setup();
     IMAP::setup();
+    POP::setup();
+    Sieve::setup();
 
     s.run();
 }
