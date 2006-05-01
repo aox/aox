87a97ce0031d0ac24e7e3b97d5db40d1207c891b
Abhijit Menon-Sen <ams@wiw.org>
2006-05-01 16:54:33 +0000

    Skip blank lines before the text of a message/rfc822 bodypart.
    p4raw-id: //oryx/src/main@51984

--- message/bodypart.cpp
+++ message/bodypart.cpp
@@ -670,6 +670,9 @@ Bodypart * Bodypart::parseBodypart( uint start, uint end,
                         bp->children(), bp, error );
     }
     else if ( ct->type() == "message" && ct->subtype() == "rfc822" ) {
+        // There are sometimes blank lines before the message.
+        while ( rfc2822[start] == 13 || rfc2822[start] == 10 )
+            start++;
         Message * m = new Message( rfc2822.mid( start, end-start ) );
         List<Bodypart>::Iterator it( m->children() );
         while ( it ) {
