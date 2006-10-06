57084885457822e9e6cdca1d750c0927244b711f
Abhijit Menon-Sen <ams@wiw.org>
2006-10-06 02:31:25 +0000

    Make the code also do what the compiler wants.
    p4raw-id: //oryx/src/main@52824

--- db/query.cpp
+++ db/query.cpp
@@ -763,7 +763,7 @@ class Log * Query::log() const
     Log * l = 0;
     if ( d->owner )
         l = d->owner->log();
-    if ( !l && d->transaction() )
+    if ( !l && d->transaction )
         l = d->transaction->owner()->log();
     return l;
 }
