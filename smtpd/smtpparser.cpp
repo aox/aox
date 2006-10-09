9ea7db2ad13c27d5d8c8c6672e3a3edc61fc77a6
Abhijit Menon-Sen <ams@wiw.org>
2006-10-09 06:14:11 +0000

    Clarify CRLF termination.
    p4raw-id: //oryx/src/main@52835

--- smtpd/smtpparser.cpp
+++ smtpd/smtpparser.cpp
@@ -11,8 +11,8 @@
 */
 
 /*! Creates a new SmtpParser object for the string \a s, which is
-    assumed to be a complete SMTP command line, as received from
-    the client.
+    assumed to be a complete SMTP command line (not including the
+    terminating CRLF), as received from the client.
 */
 
 SmtpParser::SmtpParser( const String &s )
