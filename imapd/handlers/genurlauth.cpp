fadcb2d30621502d722dcf75e8cf63b9b889acd5
Abhijit Menon-Sen <ams@wiw.org>
2006-10-18 20:56:32 +0000

    A working GENURLAUTH.
    p4raw-id: //oryx/src/main@52953

--- imapd/handlers/genurlauth.cpp
+++ imapd/handlers/genurlauth.cpp
@@ -2,8 +2,28 @@
 
 #include "genurlauth.h"
 
-#include "imapurl.h"
+#include "md5.h"
 #include "list.h"
+#include "query.h"
+#include "imapurl.h"
+#include "stringlist.h"
+#include "configuration.h"
+#include "transaction.h"
+#include "entropy.h"
+#include "mailbox.h"
+#include "user.h"
+
+
+struct UrlKey {
+    UrlKey( ImapUrl * u )
+        : q( 0 ), url( u ), mailbox( 0 )
+    {}
+
+    Query * q;
+    String key;
+    ImapUrl * url;
+    Mailbox * mailbox;
+};
 
 
 class GenUrlauthData
@@ -11,10 +31,12 @@ class GenUrlauthData
 {
 public:
     GenUrlauthData()
-        : urls( 0 )
+        : state( 0 ), urlKeys( 0 ), t( 0 )
     {}
 
-    List<ImapUrl> * urls;
+    uint state;
+    List<UrlKey> * urlKeys;
+    Transaction * t;
 };
 
 
@@ -30,24 +52,23 @@ GenUrlauth::GenUrlauth()
 
 void GenUrlauth::parse()
 {
-    d->urls = new List<ImapUrl>;
+    d->urlKeys = new List<UrlKey>;
     do {
         space();
 
         String s( astring() );
+        ImapUrl * url = new ImapUrl( s );
+        if ( !url->valid() ) {
+            error( Bad, "Invalid URL: " + s );
+            return;
+        }
         space();
         if ( !present( "INTERNAL" ) ) {
             error( Bad, "Expected INTERNAL, but saw: " + following() );
             return;
         }
 
-        ImapUrl * url = new ImapUrl( s );
-        if ( !url->valid() ) {
-            error( Bad, "Invalid URL: " + s );
-            return;
-        }
-
-        d->urls->append( url );
+        d->urlKeys->append( new UrlKey( url ) );
     }
     while ( nextChar() == ' ' );
     end();
@@ -56,5 +77,106 @@ void GenUrlauth::parse()
 
 void GenUrlauth::execute()
 {
+    if ( d->state == 0 ) {
+        uint port = Configuration::scalar( Configuration::ImapPort );
+
+        List<UrlKey>::Iterator it( d->urlKeys );
+        while ( it ) {
+            ImapUrl * u = it->url;
+            Mailbox * m = mailbox( u->mailbox() );
+
+            // XXX: We don't return an invalid URLAUTH token for invalid
+            // userids; in fact, we don't even bother to verify that any
+            // userid specified in "access" is valid.
+
+            if ( u->user() != imap()->user()->login() ||
+                 !( u->host().lower() == Configuration::hostname().lower() &&
+                    u->port() == port ) ||
+                 !m || !u->isRump() )
+            {
+                error( Bad, "Invalid URL" );
+                return;
+            }
+
+            it->mailbox = m;
+
+            ++it;
+        }
+
+        d->state = 1;
+    }
+
+    if ( d->state == 1 ) {
+        d->t = new Transaction( this );
+
+        Query * q = new Query( "lock access_keys in exclusive mode", this );
+        d->t->enqueue( q );
+
+        List<UrlKey>::Iterator it( d->urlKeys );
+        while ( it ) {
+            it->q = new Query( "select key from access_keys where userid=$1 "
+                               "and mailbox=$2", this );
+            it->q->bind( 1, imap()->user()->id() );
+            it->q->bind( 2, it->mailbox->id() );
+            d->t->enqueue( it->q );
+            ++it;
+        }
+
+        d->t->execute();
+        d->state = 2;
+    }
+
+    if ( d->state == 2 ) {
+        List<UrlKey>::Iterator it( d->urlKeys );
+        while ( it ) {
+            Query * q = it->q;
+
+            if ( !q->done() )
+                return;
+
+            if ( q->hasResults() ) {
+                Row * r = q->nextRow();
+                it->key = r->getString( "key" );
+            }
+            else if ( q->rows() == 0 ) {
+                q = new Query( "insert into access_keys "
+                               "(userid,mailbox,key) values ($1,$2,$3)",
+                               this );
+                q->bind( 1, imap()->user()->id() );
+                q->bind( 2, it->mailbox->id() );
+                q->bind( 3, Entropy::asString( 16 ).e64() );
+                d->t->enqueue( q );
+            }
+
+            ++it;
+        }
+
+        d->t->commit();
+        d->state = 3;
+    }
+
+    if ( d->state == 3 ) {
+        if ( !d->t->done() )
+            return;
+
+        if ( d->t->failed() ) {
+            error( No, "Database error: " + d->t->error() );
+            return;
+        }
+
+        StringList l;
+        List<UrlKey>::Iterator it( d->urlKeys );
+        while ( it ) {
+            String orig( it->url->orig() );
+            String u( orig );
+            u.append( ":internal:0" );
+            u.append( MD5::HMAC( it->key.de64(), orig ).hex() );
+            l.append( imapQuoted( u ) );
+            ++it;
+        }
+
+        respond( "GENURLAUTH " + l.join( " " ) );
+    }
+
     finish();
 }
