c4ecead870a0c785f7bd6a0c390579c77dcb0230
Abhijit Menon-Sen <ams@wiw.org>
2006-12-05 11:31:31 +0000

    Display a list of all threads in a mailbox (with bad links).
    p4raw-id: //oryx/src/main@53245

--- http/components/archivemailbox.cpp
+++ http/components/archivemailbox.cpp
@@ -3,6 +3,12 @@
 #include "archivemailbox.h"
 
 #include "link.h"
+#include "field.h"
+#include "frontmatter.h"
+#include "addressfield.h"
+#include "mailboxview.h"
+#include "message.h"
+#include "header.h"
 
 
 class ArchiveMailboxData
@@ -29,9 +35,78 @@ ArchiveMailbox::ArchiveMailbox( Link * link )
       d( new ArchiveMailboxData )
 {
     d->link = link;
+    addFrontMatter( FrontMatter::jsToggles() );
 }
 
 
 void ArchiveMailbox::execute()
 {
+    MailboxView * mv = MailboxView::find( d->link->mailbox() );
+    if ( !mv->ready() ) {
+        mv->refresh( this );
+        return;
+    }
+
+    if ( mv->count() == 0 ) {
+        setContents( "<p>Mailbox is empty" );
+        return;
+    }
+
+    String s;
+    List<MailboxView::Thread>::Iterator it( mv->allThreads() );
+    while ( it ) {
+        MailboxView::Thread * t = it;
+        ++it;
+        Message * m = t->message( 0 );
+        String url( d->link->canonical() );
+        if ( !url.endsWith( "/" ) )
+            url.append( "/" );
+        url.append( fn( t->uid( 0 ) ) );
+
+        HeaderField * hf = m->header()->field( HeaderField::Subject );
+        String subject;
+        if ( hf )
+            subject = hf->data().simplified();
+        if ( subject.isEmpty() )
+            subject = "(No Subject)";
+        s.append( "<div class=thread>\n"
+                  "<div class=headerfield>Subject: " );
+        s.append( quoted( subject ) );
+        s.append( "</div>\n" ); // subject
+
+        s.append( "<div class=threadcontributors>\n" );
+        s.append( "<div class=headerfield>From:\n" );
+        uint i = 0;
+        while ( i < t->messages() ) {
+            m = t->message( i );
+            s.append( "<a href=\"" );
+            s.append( url );
+            if ( i > 0 ) {
+                s.append( "#" );
+                s.append( fn( t->uid( i ) ) );
+            }
+            s.append( "\">" );
+            AddressField * af
+                = m->header()->addressField( HeaderField::From );
+            if ( af ) {
+                List< Address >::Iterator it( af->addresses() );
+                while ( it ) {
+                    s.append( address( it ) );
+                    ++it;
+                    if ( it )
+                        s.append( ", " );
+                }
+            }
+            s.append( "</a>" );
+            i++;
+            if ( i < t->messages() )
+                s.append( "," );
+            s.append( "\n" );
+        }
+        s.append( "</div>\n" // headerfield
+                  "</div>\n" // threadcontributors
+                  "</div>\n" ); // thread
+    }
+
+    setContents( s );
 }
