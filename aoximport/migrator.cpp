30f292f08706ec4b7d1ad34b2c21c39bfeffcb08
Arnt Gulbrandsen <arnt@oryx.com>
2007-01-17 18:11:23 +0000

    a sensible default internalDate() for migrated messages
    p4raw-id: //oryx/src/main@53344

--- aoximport/migrator.cpp
+++ aoximport/migrator.cpp
@@ -15,6 +15,7 @@
 #include "maildir.h"
 #include "cyrus.h"
 #include "mbox.h"
+#include "date.h"
 #include "mh.h"
 
 #include <stdio.h>
@@ -300,6 +301,9 @@ MigratorMessage::MigratorMessage( const String & rfc822, const String & desc )
     m = new Message( o );
     if ( m->error().isEmpty() )
         return;
+    if ( m->header() &&
+         m->header()->date() )
+        m->setInternalDate( m->header()->date()->unixTime() );
 
     if ( Migrator::verbosity() > 0 )
         fprintf( stdout, "Message %s: Working around error: %s\n",
