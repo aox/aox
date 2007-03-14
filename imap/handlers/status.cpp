f75423fe577f3597414379d210a290bfa4066a1c
Arnt Gulbrandsen <arnt@oryx.com>
2007-03-14 14:22:11 +0000

    make sure the session is completely up to date before asking it how many messages are available.
    p4raw-id: //oryx/src/main@53555

--- imap/handlers/status.cpp
+++ imap/handlers/status.cpp
@@ -156,6 +156,13 @@ void Status::execute()
     // third part: return the payload.
     StringList status;
 
+    // session hasn't told anyone about its messages, so it won't
+    // admit to their existence. since it's a Session, not an
+    // ImapSession, emitResponses() won't cause output, so we just
+    // force it.
+    if ( d->session )
+        d->session->emitResponses( Session::New );
+
     if ( d->messages )
         status.append( "MESSAGES " + fn( d->session->count() ) );
     if ( d->recent )
