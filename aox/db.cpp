d3ee86e08386920cf7986be04315e2fe968c452f
Abhijit Menon-Sen <ams@wiw.org>
2008-01-16 02:36:10 +0000

    Another syntax error.
    p4raw-id: //oryx/src/main@54768

--- aox/db.cpp
+++ aox/db.cpp
@@ -159,7 +159,7 @@ void Vacuum::execute()
                        "where id in "
                        "(select message from deleted_messages dm"
                        " where dm.deleted_at<current_timestamp-'" +
-                       fn( days ) + " days'::interval "
+                       fn( days ) + " days'::interval) "
                        "and id not in (select message from deliveries) "
                        "and id not in (select message from mailbox_messages)",
                        0 );
