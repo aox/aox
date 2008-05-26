9095669cb317bffb7bf2ba8b0bf0930427b24728
Arnt Gulbrandsen <arnt@libertango.oryx.com>
2008-05-26 13:59:35 +0200

    the first of what we need to Listen/Notify

---
+++ db/dbsignal.cpp
@@ -0,0 +1,68 @@
+// Copyright Oryx Mail Systems GmbH. All enquiries to info@oryx.com, please.
+
+#include "dbsignal.h"
+
+#include "event.h"
+#include "scope.h"
+#include "list.h"
+
+
+static List<DatabaseSignal> * signals = 0;
+
+
+class DatabaseSignalData
+    : public Garbage
+{
+public:
+    DatabaseSignalData(): o( 0 ) {}
+    String n;
+    EventHandler * o;
+};
+
+
+/*! \class DatabaseSignal dbsignal.h
+
+    The DatabaseSignal class provides an interface to the PostgreSQL
+    LISTEN command. By creating an instance of this class, you request
+    to be notified whenever anyone uses the corresponsing gp NOTIFY
+    command.
+
+    This is an eternal object. Once you've done this, there is no
+    turning back. The listening never stops.
+*/
+
+
+/*! Constructs a DatabaseSignal for \a name which will notified \a
+    owner. Forever.
+*/
+
+DatabaseSignal::DatabaseSignal( const String & name, EventHandler * owner )
+    : Garbage(), d( new DatabaseSignalData )
+{
+    d->n = name;
+    d->o = owner;
+    if ( !signals ) {
+        signals = new List<DatabaseSignal>;
+        Allocator::addEternal( signals, "database notify/listen listeners" );
+    }
+    signals->append( this );
+}
+
+
+/*! This command should be called only by Postgres. It notifies those
+    event handlers who have created DatabaseSignal objects for \a
+    name.
+*/
+
+void DatabaseSignal::notifyAll( const String & name )
+{
+    List<DatabaseSignal>::Iterator i( signals );
+    while ( i ) {
+        DatabaseSignal * s = i;
+        ++i;
+        if ( name == s->d->n && s->d->o ) {
+            Scope x( s->d->o->log() );
+            s->d->o->execute();
+        }
+    }
+}
---
+++ db/dbsignal.h
@@ -0,0 +1,27 @@
+// Copyright Oryx Mail Systems GmbH. All enquiries to info@oryx.com, please.
+
+#ifndef DBSIGNAL_H
+#define DBSIGNAL_H
+
+#include "string.h"
+
+class EventHandler;
+
+
+class DatabaseSignal
+    : public Garbage
+{
+public:
+    DatabaseSignal( const String &, EventHandler * );
+
+    static void notifyAll( const String & );
+
+private: // noone can destroy this
+    ~DatabaseSignal() {}
+
+private:
+    class DatabaseSignalData * d;
+};
+
+
+#endif
