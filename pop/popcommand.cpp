ef9a650a086d62d59e04c2412460ca0bee007c19
Abhijit Menon-Sen <ams@oryx.com>
2008-06-09 15:01:28 +0530

    Backport c57f25664a77dde6c3fa7a0643cd094cb6c08f7a to fix POP.

--- pop/popcommand.cpp
+++ pop/popcommand.cpp
@@ -372,6 +372,7 @@ bool PopCommand::session()
         return false;
 
     d->session->clearExpunged();
+    d->session->emitResponses( Session::New );
     d->pop->setState( POP::Transaction );
     d->pop->ok( "Done" );
     return true;
