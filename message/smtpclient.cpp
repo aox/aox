5455f587569e53af316143b6fb52f62512ad1e09
Arnt Gulbrandsen <arnt@oryx.com>
2008-09-08 11:14:45 +0200

    make SURE we use only asfd@as.df as mail from... shouldn't matter, the address we use as sender ought never to have a display-name. but it's better to be sure.

--- message/smtpclient.cpp
+++ message/smtpclient.cpp
@@ -229,7 +229,7 @@ void SmtpClient::sendCommand()
             return;
         send = "mail from:<";
         if ( d->dsn->sender()->type() == Address::Normal )
-            send.append( d->dsn->sender()->toString() );
+            send.append( d->dsn->sender()->lpdomain() );
         send.append( ">" );
 
         d->state = SmtpClientData::MailFrom;
