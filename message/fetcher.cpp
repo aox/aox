eff17c7d6db8e78dfd4ccbb84ecd6d9f9ade441e
Arnt Gulbrandsen <arnt@oryx.com>
2008-11-21 11:09:05 +0100

    try to get one batch every 15 seconds isntead of every 90
    90 seconds means a lot of messages means a lot of RAM

--- message/fetcher.cpp
+++ message/fetcher.cpp
@@ -361,7 +361,7 @@ void Fetcher::waitForEnd()
 
 /*! Messages are fetched in batches, so that we can deliver some rows
     early on. This function adjusts the size of the batches so we'll
-    get about one batch every 90 seconds, and updates the tables so we
+    get about one batch every 15 seconds, and updates the tables so we
     have a batch ready for reading.
 */
 
@@ -382,9 +382,9 @@ void Fetcher::prepareBatch()
         }
         else {
             // we adjust the batch size so the next batch could take
-            // something in the approximate region of 90 seconds.
+            // something in the approximate region of 15 seconds.
             uint diff = now - d->lastBatchStarted;
-            d->batchSize = d->batchSize * 90 / diff;
+            d->batchSize = d->batchSize * 15 / diff;
         }
         if ( d->batchSize > prevBatchSize * 3 )
             d->batchSize = prevBatchSize * 3;
@@ -394,7 +394,9 @@ void Fetcher::prepareBatch()
             d->batchSize = 128;
         if ( d->batchSize > d->maxBatchSize )
             d->batchSize = d->maxBatchSize;
-        if ( Postgres::version() < 80200 && d->batchSize > 50 && d->otherheader )
+        if ( Postgres::version() < 80200 &&
+             d->batchSize > 50 &&
+             d->otherheader )
             d->batchSize = 50;
         if ( prevBatchSize != d->batchSize )
             log( "Batch time was " + fn ( now - d->lastBatchStarted ) +
