36db85f13a073216a5f048e70893d589cecb5e42
Arnt Gulbrandsen <arnt@oryx.com>
2009-02-20 14:18:41 +0100

    be as strict as possible about what might be a boundary line

--- core/estring.cpp
+++ core/estring.cpp
@@ -1306,11 +1306,32 @@ static bool maybeBoundary( const EString & s, uint i ) {
         return false;
     if ( s[i] != '-' || s[i+1] != '-' )
         return false;
+    
     while ( i < s.length() && s[i] >= ' ' ) {
-        if ( s[i] == ' ' )
+        //bchars := bcharsnospace / " "
+        //bcharsnospace := DIGIT / ALPHA / "'" / "(" / ")" /
+        //                 "+" / "_" / "," / "-" / "." /
+        //                 "/" / ":" / "=" / "?"
+        switch( s[i] ) {
+        case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
+        case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
+        case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
+        case 'v': case 'w': case 'x': case 'y': case 'z':
+        case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
+        case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
+        case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
+        case 'V': case 'W': case 'X': case 'Y': case 'Z':
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6':
+        case '7': case '8': case '9':             
+        case '\'':
+        case '(': case ')': case '+': case '_': case ',': case '-': case '.':
+        case '/': case ':': case '=': case '?':
+        case ' ':
+            // ok
+            break;
+        default:
             return false;
-        if ( s[i] == '_' )
-            return false;
+        }
         ++i;
     }
     return true;
