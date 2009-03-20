8001ae6341393dd0f281f77bec916fb711295e23
Arnt Gulbrandsen <arnt@oryx.com>
2009-03-20 10:18:35 +0100

    disable TLS as extension, too

--- server/tls.cpp
+++ server/tls.cpp
@@ -77,6 +77,7 @@ void TlsServerData::Client::react( Event e )
             log( "TlsServer: Error while connecting to tlsproxy. "
                  "Shutting down TLS.",
                  Log::Significant );
+            ::tlsAvailable = false;
             EventLoop::global()->shutdownSSL();
         }
         else {
