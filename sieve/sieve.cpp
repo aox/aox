771a24416c8055b8435abb5ab4a5facd5b214675
Arnt Gulbrandsen <arnt@oryx.com>
2010-02-15 14:36:36 +0100

    tweak f=f heuristic

--- sieve/sieve.cpp
+++ sieve/sieve.cpp
@@ -638,9 +638,8 @@ static UString magicallyFlowed( const UString & s ) {
     UString r;
     uint i = 0;
     while ( i < s.length() ) {
-        if ( i > 0 && s[i] == '\n' &&
-             !( s[i-1] == '\n' || s[i-1] == '\r' ) &&
-             !( s[i+1] == '\n' || s[i+1] == '\r' ) )
+        if ( i > 0 && s[i] == '\r' && s[i+1] = '\n' &&
+             s[i-1] != '\n' && s[i+2] != '\r' )
             r.append( 32 );
         r.append( s[i] );
         i++;
