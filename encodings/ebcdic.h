1c253d90fc191948ceeeda7b24efbe786721d9ab
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
2010-05-11 15:57:09 +0200

    EBCDIC support.
    I am told that all software evolves until it can read mail. Clearly aox
    can read mail, so it needs a different goal. EBCDIC.

---
+++ encodings/ebcdic.h
@@ -0,0 +1,17 @@
+// Copyright Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>.
+
+#ifndef EBCDIC_H
+#define EBCDIC_H
+
+#include "codec.h"
+
+
+class EbcdicCodec: public TableCodec {
+public:
+    EbcdicCodec();
+};
+
+
+
+
+#endif
