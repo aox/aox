4547386af03942452fba08c584bf2dc513aeb6d1
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
2010-06-23 21:58:45 +0200

    the code implements T=OS, so until I see a bug...

--- imap/handlers/capability.cpp
+++ imap/handlers/capability.cpp
@@ -118,6 +118,7 @@ EString Capability::capabilities( IMAP * i, bool all )
     if ( Configuration::toggle( Configuration::UseTls ) && !i->hasTls() )
         c.append( "STARTTLS" );
     if ( all || login ) {
+        c.append( "THREAD=ORDEREDSUBJECT" );
         c.append( "THREAD=REFS" );
         c.append( "THREAD=REFERENCES" );
         c.append( "UIDPLUS" );
