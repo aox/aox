aox/aox

commit ee5308a9bf789e49b1c05bc5fab4fc74739b2165

detect outlook, and enable the NAT defeater for it.
the heuristic is far from perfect: any IMAP client whose tags are all four
letters long is Outlook. the last tag tested is that for the
authentication command.

imap/imap.cpp
@@ -531,6 +531,17 @@ void IMAP::setUser( User * user, const EString & mechanism )
     setState( Authenticated );
     if ( Configuration::toggle( Configuration::AutoFlagViews ) )
         (void)new FlagViewsCreator( user );
+
+    bool possiblyOutlook = true;
+    List< Command >::Iterator i( d->commands );
+    while ( i && possiblyOutlook ) {
+        EString tag = i->tag();
+        ++i;
+        if ( tag.length() != 4 || tag.contains( '.' ) )
+            possiblyOutlook = false;
+    }
+    if ( possiblyOutlook )
+        setClientBug( Nat )
 }
 
 
@@ -551,9 +562,10 @@ void IMAP::reserve( Command * command )
 
 void IMAP::unblockCommands()
 {
-    while ( d->commands.firstElement() &&
-            d->commands.firstElement()->state() == Command::Retired )
-        d->commands.shift();
+    if ( d->state != NotAuthenticated )
+        while ( d->commands.firstElement() &&
+                d->commands.firstElement()->state() == Command::Retired )
+            d->commands.shift();
     if ( d->runningCommands )
         d->runCommandsAgain = true;
     else

commit cc23c6bc44a55890cebe1fb856885e218c18b582

Fix hang/loop when imap authentication fails
If a new imap session is not successfully setup after the first command,
aox will hang in an infinite loop.

This is a regression caused by ee5308a.

imap/imap.cpp
@@ -562,10 +562,9 @@ void IMAP::reserve( Command * command )
 
 void IMAP::unblockCommands()
 {
-    if ( d->state != NotAuthenticated )
-        while ( d->commands.firstElement() &&
-                d->commands.firstElement()->state() == Command::Retired )
-            d->commands.shift();
+    while ( d->commands.firstElement() &&
+            d->commands.firstElement()->state() == Command::Retired )
+        d->commands.shift();
     if ( d->runningCommands )
         d->runCommandsAgain = true;
     else