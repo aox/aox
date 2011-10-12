e190a285027d4794b35240fbb13d3f7d14da7bab
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
2011-10-12 13:35:27 +0200

    we want the byte 1. ntohs() is inapplicable to bytes.

--- core/configuration.cpp
+++ core/configuration.cpp
@@ -670,7 +670,7 @@ void Configuration::setup( const EString & global, bool allowFailure )
                 in6.sin6_addr.s6_addr[i] = 0;
                 ++i;
             }
-            in6.sin6_addr.s6_addr[15] = ntohs( 1 );
+            in6.sin6_addr.s6_addr[15] = 1;
             in6.sin6_scope_id = 0;
             if ( ::bind( s, (struct sockaddr *)&in6, sizeof( in6 ) ) < 0 ) {
                 if ( errno == EADDRINUSE )
