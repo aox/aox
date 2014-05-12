63d4cebaa06483e9e72f6614e5e047552a8d6777
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
2014-05-12 14:46:31 +0200

    be compatible with older versions of g++

--- core/md5.cpp
+++ core/md5.cpp
@@ -149,12 +149,9 @@ EString MD5::hash()
     }
     swapBytes( in, 14 );
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wstrict-aliasing"
     /* Append length in bits and transform. */
     ((uint32 *)in)[14] = bits[0];
     ((uint32 *)in)[15] = bits[1];
-#pragma GCC diagnostic pop
     transform();
     swapBytes( (char *)buf, 4 );
 
